39bcc21f440a1b69975e06a1daada459299c7ecc
Gerrit Holl <gerrit.holl@dwd.de>
2023-03-16 15:23:55 +0100

    It doesn't work.

--- satpy/composites/atmosphere.py
+++ satpy/composites/atmosphere.py
@@ -20,6 +20,9 @@ try:
 except ImportError:
     metpy = None
 
+import dask
+import xarray as xr
+
 from satpy.composites import CompositeBase
 
 
@@ -27,6 +30,24 @@ def _ensure_metpy():
     """Make sure that metpy is there."""
     if metpy is None:
         raise ImportError("Please install metpy to derive quantities from atmospheric profiles.")
+
+
+@dask.delayed
+def _calculate_cape_cin(air_pressure, temperature, dewpoint):
+    """Calculate CAPE and CIN through dask."""
+    all_cc = []
+    for x in air_pressure.x:
+        for y in air_pressure.y:
+            p = air_pressure.sel(x=x, y=y)
+            t = temperature.sel(x=x, y=y)
+            d = dewpoint.sel(x=x, y=y)
+            # needs https://github.com/hgrecco/pint/pull/1722 to work!
+            pp = dask.delayed(metpy.calc.parcel_profile)(p[::-1], t[-1], d[-1])
+            cc = dask.delayed(metpy.calc.cape_cin)(p[::-1], t[::-1], d[::-1], pp)[0]
+            all_cc.append(
+                    cc.metpy.dequantify.expand_dims(dim={"x": 1, "y": 1}).
+                    assign_coords({"x": [x], "y": [x]}))
+    return dask.delayed(xr.concat)(all_cc, dim="x").compute()
 
 
 class CAPE(CompositeBase):
@@ -45,11 +66,12 @@ class CAPE(CompositeBase):
 
         # product contains the specific humidity (source:
         # PUG EUM/USC/DOC/22/1281633, § 4.1
-        dewp = metpy.calc.dewpoint_from_specific_humidity(
+        dewpoint = metpy.calc.dewpoint_from_specific_humidity(
                 air_pressure, temperature,
                 metpy.calc.mixing_ratio_from_specific_humidity(specific_humidity))
 
-        p = air_pressure.sel(x=50, y=400)
-        t = temperature.sel(x=50, y=400)
-        d = dewp.sel(x=50, y=400)
-        metpy.calc.parcel_profile(p[::-1], t[-1], d[-1])
+        # calculating CAPE and CIN is neither vectorised nor dask friendly, so
+        # use a dask delayed wrapper
+
+        cape = _calculate_cape_cin(air_pressure, temperature, dewpoint)
+        return cape.compute()
